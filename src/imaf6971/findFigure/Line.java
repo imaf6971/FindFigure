32943ea932be1aa09d7e7782c025dde2a2e01a27
imaf6971 <imaf6971@gmail.com>
2021-02-15 20:58:52 +0300

    Main class is good

--- src/imaf6971/findFigure/Line.java
+++ src/imaf6971/findFigure/Line.java
@@ -4,7 +4,7 @@ public class Line {
     private double length;
 
     public Line(Dot A, Dot B) {
-        length = Math.sqrt(Math.pow(B.getX() - A.getX(), 2) - Math.pow(B.getY() - A.getY(), 2));
+        length = Math.sqrt(Math.pow(B.getX() - A.getX(), 2) + Math.pow(B.getY() - A.getY(), 2));
     }
 
     public double getLength() {
@@ -14,7 +14,7 @@ public class Line {
     @Override
     public String toString() {
         return "Line{" +
-                "length=" + length +
+                "length=" + getLength() +
                 '}';
     }
 }
--- src/imaf6971/findFigure/Main.java
+++ src/imaf6971/findFigure/Main.java
@@ -7,29 +7,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //Scanner init
 
         List<Dot> dotList = new ArrayList<>();
-        List<Line> lineList = new ArrayList<>();
-
         for (int i = 0; i < 4; i++) {
-            System.out.println("Введите координаты точки " + i);
+            int j = i + 1;
+            System.out.println("Input dot number " + j + " coordinate (x, y): ");
+
             double x = scanner.nextDouble();
             double y = scanner.nextDouble();
-            dotList.add(i, new Dot(x, y));
-            System.out.println();
+
+            Dot newDot = new Dot(x, y);
+            dotList.add(i, newDot);
         }
 
+        List<Line> lineList = new ArrayList<>();
 
-        for (int i = 0; i < 4; i++) {
-            lineList.add(i, new Line(dotList.get(i), dotList.get(i + 1)));
-            System.out.println(lineList.get(i));
+        for (int i = 0; i < 3; i++) {
+            lineList.add(new Line(dotList.get(i), dotList.get(i + 1)));
         }
+        lineList.add(new Line(dotList.get(3), dotList.get(0)));
+        lineList.add(new Line(dotList.get(0), dotList.get(2)));
+        lineList.add(new Line(dotList.get(1), dotList.get(3)));
 
-        for (int i = 0; i < 2; i++) {
-            lineList.add(i + 4, new Line(dotList.get(i), dotList.get(i + 3)));
-        }
+        Figure newFigure = new Figure(lineList.get(0), lineList.get(1), lineList.get(2), lineList.get(3), lineList.get(4), lineList.get(5));
+        System.out.println("Your figure type is: " + newFigure.getFigureType());
 
-        System.out.println("Hello again");
     }
 }
